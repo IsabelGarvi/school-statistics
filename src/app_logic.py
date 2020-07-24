72d189dc447f5e6aeb68a194ee543360c7793346
Izz Garvi <igarvilo@gmail.com>
2020-07-24 16:11:09 +0100

    user interaction

--- src/app_logic.py
+++ src/app_logic.py
@@ -1,7 +1,23 @@
+import os
 import sys
 from typing import List
 
 from src.database_manager import SchoolDB
+
+db_name = os.getenv("DB_NAME")
+db_user = os.getenv("DB_USER")
+db_pass = os.getenv("DB_PASS")
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+
+
+school_data_manager = SchoolDB(
+    db_name=db_name,
+    db_user=db_user,
+    db_pass=db_pass,
+    db_host=db_host,
+    db_port=db_port,
+)
 
 
 def get_percentage_failed(subject: str, year: str) -> float:
--- tests/test_app_logic.py
+++ tests/test_app_logic.py
@@ -1,3 +1,5 @@
+import os
+
 from src import app_logic
 
 
