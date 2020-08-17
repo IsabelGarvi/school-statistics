36d5078a9617064a4992e6875d4b26b9a9979959
Izz Garvi <igarvilo@gmail.com>
2020-08-17 15:47:14 +0100

    tests added

--- src/excel_file_processing.py
+++ src/excel_file_processing.py
@@ -2,28 +2,19 @@ from typing import List
 import xlrd as xlrd
 
 
-def get_workbook_from_excel_file(file) -> None:
-    """
-    Opens the file passed by parameter and processes it as an Excel file.
-    Gets all the sheets in the Excel file.
-
-    :param file: file to be processed
-    """
-
-    return xlrd.open_workbook(filename=file)
-
-
-def get_data_from_workbook(workbook):
-    sheet_name_and_year = []
-
+def extract_data_from_file(file):
+    workbook = xlrd.open_workbook(filename=file)
     for sheet in workbook.sheets():
-        sheet_name_and_year.append(get_sheet_name_and_year(sheet))
+        subject_name, year = _get_sheet_name_and_year(sheet=sheet)
+    pass
 
 
-def get_sheet_name_and_year(sheet) -> List[tuple]:
+def _get_sheet_name_and_year(sheet) -> tuple:
     subject, course, year = sheet.name.split()
     return subject + " " + course, year
 
 
-def get_columns_name_from_sheet():
-    pass
+def _get_columns_data_from_sheet(sheet):
+    for row_index in range(1, sheet.nrows):
+        row_values = sheet.row_values(row_index)
+        print(row_values)
--- tests/test_extract_data_from_excel_file.py
+++ tests/test_extract_data_from_excel_file.py
@@ -1,24 +1,34 @@
 import xlrd
 from src import excel_file_processing
+from itertools import product
+from unittest.mock import patch
 
 file = "files/test-input.xlsx"
 
 
+# @patch('src.excel_file_processing._get_sheet_name_and_year')
 def test_get_data_from_workbook():
+    excel_file_processing.extract_data_from_file(file=file)
     excel_wb = xlrd.open_workbook(filename=file)
-    pass
+    first_sheet = excel_wb.sheet_by_index(0)
+    with patch.object(
+        excel_file_processing,
+        "_get_sheet_name_and_year",
+        return_value=("Lit 3", "2019-2020"),
+    ) as _get_sheet_name_and_year_mock:
+        excel_file_processing._get_sheet_name_and_year(sheet=first_sheet)
+
+    _get_sheet_name_and_year_mock.assert_called()
 
 
 def test_get_subject_name_and_year():
     excel_wb = xlrd.open_workbook(filename=file)
     first_sheet = excel_wb.sheet_by_index(0)
 
-    stuff = excel_file_processing.get_sheet_name_and_year(sheet=first_sheet)
+    stuff = excel_file_processing._get_sheet_name_and_year(sheet=first_sheet)
 
     assert stuff == ("Lit 3", "2019-2020")
 
 
-# def test_get_column_value():
-#     excel_wb = xlrd.open_workbook(filename=file)
-#     for sheet in excel_wb.sheets():
-#         print(sheet.col_values(columnindex))
+def test_get_row_value():
+    pass
