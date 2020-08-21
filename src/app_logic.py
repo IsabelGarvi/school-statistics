83f63ab2f5f566e094c6071fcadb656e4090142e
Izz Garvi <igarvilo@gmail.com>
2020-08-21 15:27:14 +0100

    docstrings on app_logic

--- src/app_logic.py
+++ src/app_logic.py
@@ -20,7 +20,16 @@ school_data_manager = SchoolDB(
 )
 
 
-def store_student_data(subject_name, year, student_data) -> None:
+def store_student_data(
+    subject_name: str, year: str, student_data: List[list]
+) -> None:
+    """Call for storing the student data into the db for each element of student_data.
+
+    Args:
+        subject_name (str): name of the subject.
+        year (str): year in which the subject has been taught.
+        student_data (List[list]): Data of the students of that subject on that given year.
+    """
     for student in student_data:
         school_data_manager.store_data_in_db(
             name=student[0],
@@ -32,6 +41,15 @@ def store_student_data(subject_name, year, student_data) -> None:
 
 
 def get_percentage_failed(subject: str, year: str) -> float:
+    """Calculate the percentage of students that failed a subject on a given year getting the data from the database.
+
+    Args:
+        subject (str): Name of the subject to get the information from.
+        year (str): Year in which the subject was taught.
+
+    Returns:
+        float: Percentage of students that failed the subject on year passed as a parameter.
+    """
     failed = school_data_manager.get_number_failed_by_subject_and_year(
         subject=subject, year=year
     )
@@ -48,6 +66,15 @@ def get_percentage_failed(subject: str, year: str) -> float:
 
 
 def get_percentage_passed(subject: str, year: str) -> float:
+    """Calculate the percentage of students that passed a subject on a given year getting the data from the database.
+
+    Args:
+        subject (str): Name of the subject to get the information from.
+        year (str): Year in which the subject was taught.
+
+    Returns:
+        float: Percentage of students that passed the subject on year passed as a parameter.
+    """
     passed = school_data_manager.get_number_passed_by_subject_and_year(
         subject=subject, year=year
     )
@@ -64,16 +91,42 @@ def get_percentage_passed(subject: str, year: str) -> float:
 
 
 def get_list_students_in_subject(subject: str, year: str) -> List[str]:
+    """Get a list of students that were enrolled in a subject on a given year.
+
+    Args:
+        subject (str): Name of the subject to get the information from.
+        year (str): Year in which the subject was taught.
+
+    Returns:
+        List[str]: List of students' name and last name that were enrolled in the subject on year passed.
+    """
     return school_data_manager.get_list_students_by_subject_and_year(
         subject=subject, year=year
     )
 
 
 def get_total_number_students_in_subject(subject: str, year: str) -> int:
+    """Get the total number of students that were enrolled in a subject on a given year.
+
+    Args:
+        subject (str): Name of the subject to get the information from.
+        year (str): Year in which the subject was taught.
+
+    Returns:
+        int: Total number of students enrolled in the subject on the year passed as a parameter.
+    """
     return school_data_manager.get_number_students_by_subject_and_year(
         subject=subject, year=year
     )
 
 
 def get_list_subjects_in_year(year: str) -> List[str]:
+    """Get the list of subject taught in a given year.
+
+    Args:
+        year (str): Year from which we want the information.
+
+    Returns:
+        List[str]: Subjects that where taught in the year passed as parameter.
+    """
     return school_data_manager.get_list_subjects_by_year(year=year)
